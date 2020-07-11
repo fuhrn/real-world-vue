28df11f0a90865afa22b11fa067c0333d1bf1376
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-07-11 12:35:31 -0300

    State & Getters

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,47 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr'},
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      {id:1, text: '.....', done: true},
+      {id:1, text: '.....', done: false},
+      {id:1, text: '.....', done: true},
+      {id:1, text: '.....', done: true},
+    ],
+    events: [
+      {id:1, title: '.....', organizer: true},
+      {id:2, title: '.....', organizer: true},
+      {id:3, title: '.....', organizer: true},
+      {id:4, title: '.....', organizer: true},
+    ]
+  },
   mutations: {},
   actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state) => {
+      // return state.todos.length - getters.doneTodos.length
+      return state.todos.filter(todo => !todo.done).length // no necesito pasar getters a activeTodosCoutn
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  },
+
   modules: {}
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,10 +1,32 @@
 <template>
-    <h1>Create Event</h1>
+    <div>
+        <h1>Create an Event, {{ user.name }}</h1>
+        <p>This event was created by {{ user.name }}</p>
+        <p>There are {{ catLength }} categories</p>
+        <p>{{ getEventById(2) }}</p>
+<!--        <ul>-->
+<!--            <li v-for="cat in categories" :key="cat">{{ cat }}</li>-->
+<!--        </ul>-->
+    </div>
 </template>
 
 <script>
+    import {mapState, mapGetters } from 'vuex'
+
     export default {
-        name: "EventCreate"
+        computed: {
+            ...mapGetters(["getEventById"]), //accediendo via MapGetters
+
+            //accediendo a los getters del state directamente
+            // getEvent() {
+            //   return this.$store.getters.getEventById
+            // },
+            
+            catLength() {
+                return this.$store.getters.catLength //accediendo directamente al state
+            },
+            ...mapState(['user', 'categories']) //accediendo al state via mapState
+        }
     }
 </script>
 
